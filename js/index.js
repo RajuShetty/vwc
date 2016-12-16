47f69d1642156d644ee06e73307fdf7c999228c3
RajuShetty <rajushetty.b@gmail.com>
2016-12-17 00:08:18 +0530

    Added data call back of pushbots

--- js/index.js
+++ js/index.js
@@ -15,7 +15,21 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-window.plugins.PushbotsPlugin.initialize("584ccdb04a9efad7448b4567", {"android":{"sender_id":"286516895302"}});
+    window.plugins.PushbotsPlugin.initialize("584ccdb04a9efad7448b4567", {"android":{"sender_id":"286516895302"}});
+	
+	// Should be called once app receive the notification only while the application is open or in background
+    window.plugins.PushbotsPlugin.on("notification:received", function(data){
+	console.log("received:" + JSON.stringify(data));
+	
+	//Silent notifications Only [iOS only]
+	//Send CompletionHandler signal with PushBots notification Id
+	window.plugins.PushbotsPlugin.done(data.pb_n_id);
+    });
+
+// Should be called once the notification is clicked
+       window.plugins.PushbotsPlugin.on("notification:clicked", function(data){
+	   console.log("clicked:" + JSON.stringify(data));
+       });
 
     },
     // Update DOM on a Received Event
