c1c6bec83e3a300e5ffcbaef00e8f694c44e37bd
RajuShetty <rajushetty.b@gmail.com>
2017-09-23 20:37:54 +0530

    push not working

--- js/index.js
+++ js/index.js
@@ -33,39 +33,82 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		//senderID: Get Google senderID from Google console
-var push = PushNotification.init({ "android": {"senderID": "1058053964614"},
-"ios": {"alert": "true", "badge": "true", "sound": "true"}, "windows": {} } );
+		
+		var push = PushNotification.init({
+	android: {
+	},
+	ios: {
+		alert: "true",
+		badge: "true",
+		sound: "true"
+	},
+	windows: {}
+});
 
 push.on('registration', function(data) {
+	console.log(data.registrationId);
 	var deviceToken = data.registrationId;
 	$.ajax({
-		"url": "http://vineyardworkerschurch.org/push-vwc/",
-		"dataType": "json",
-		"method": "POST",
-		"data": {
-			"device_token" : deviceToken,
-			"device_type" : 'android',
-			"channels_id" : '1,2'
-		},
-		"success": function(response) {
-			console.log("Device ID "+deviceToken+" sent successfuly");
-		}
-	});
+        "url": "http://vineyardworkerschurch.org/?smpushcontrol=savetoken",
+        "dataType": "json",
+        "method": "POST",
+        "data": {
+            "device_token" : deviceToken,
+            "device_type" : 'android'
+        },
+        "success": function(response) {
+            console.log("Device ID "+deviceToken+" sent successfuly");
+        }
+    });
 });
 
 push.on('notification', function(data) {
-	 data.message,
-	 data.title,
-	 data.count,
-	 data.sound,
-	 data.image,
-	 data.additionalData
+	data.message,
+	data.title,
+	data.count,
+	data.sound,
+	data.image,
+	data.additionalData
+	console.log(data);
 	alert(data.message);
 });
 
 push.on('error', function(e) {
+	console.log(e.message);
+});
+		
+push.on('error', function(e) {
 	console.log("Error");
 });
+	
+	push.on('notification', function(data) {
+     console.log('notification event');
+     var cards = document.getElementById("cards");
+     var push = '<div class="row">' +
+       '<div class="col s12 m6">' +
+       '  <div class="card darken-1">' +
+       '    <div class="card-content black-text">' +
+       '      <span class="card-title black-text">' + data.title + '</span>' +
+       '      <p>' + data.message + '</p>' +
+       '      <p>' + data.additionalData.foreground + '</p>' +
+       '    </div>' +
+       '  </div>' +
+       ' </div>' +
+       '</div>';
+     cards.innerHTML += push;
+
+     app.push.finish(function() {
+         console.log('success');
+     }, function() {
+         console.log('error');
+     });
+ });	
+		
+ 
+ 
+        console.log('deviceready event');
+        document.getElementById('regId').innerHTML = 'true';
     }
 };
+
+ app.initialize();
