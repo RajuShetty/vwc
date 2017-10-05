cb76e74970b50f80a7281878a59a30925e12b4cd
RajuShetty <rajushetty.b@gmail.com>
2017-10-05 16:45:59 +0530

    back to GCM

--- server/fcmService.js
+++ server/fcmService.js
@@ -1,30 +1,6 @@
 // Load modules
 
-var FCM = require('fcm-node');
-// Replace these with your own values.
-var apiKey ="AAAA9ljuQ0Y:APA91bHhPYlRhNBazmHO7S8cdF_QN3woEVSG5L5qLUJEBgwF1F3vwW3GY5_uy2DuxmJILmge29AY7NI5BfFC-kbPfTO6RejbJ8P1BIjG0W8emHyB7vhsDwjqLZPfqIVdp03UNaEMG5FF";
-var deviceID = "my device id";
-var fcm = new FCM(apiKey);
-
-var message = {
-    "to": deviceID,
-    "data": {
-        "info": 'super secret info',
-       
-		"force-start": '1'
-    }
-};
-
-fcm.send(message, function(err, response){
-  if (err) {
-    console.log(err);
-    console.log("Something has gone wrong!");
-  } else {
-    console.log("Successfully sent with response: ", response);
-  }
-});
-
-/*var gcm = require('node-gcm');
+var gcm = require('node-gcm');
 
 // Replace these with your own values.
 var apiKey = "AAAA9ljuQ0Y:APA91bHhPYlRhNBazmHO7S8cdF_QN3woEVSG5L5qLUJEBgwF1F3vwW3GY5_uy2DuxmJILmge29AY7NI5BfFC-kbPfTO6RejbJ8P1BIjG0W8emHyB7vhsDwjqLZPfqIVdp03UNaEMG5FF";
@@ -35,10 +11,9 @@ var message = new gcm.Message();
 message.addData('title', 'Hello, World');
 message.addData('body', 'This is a notification that will be displayed ASAP.');
 message.addData('content-available', '1');
-message.addData('force-start', '1');
+message.addData('force-start', 1);
 
 service.send(message, { registrationTokens: [ deviceID ] }, function (err, response) {
 	if(err) console.error(err);
 	else 	console.log(response);
 });
-*/
