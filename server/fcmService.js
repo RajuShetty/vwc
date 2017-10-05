6d2bfc40c7e6e4f9e7c7345932386e413ce339c2
RajuShetty <rajushetty.b@gmail.com>
2017-10-05 15:44:57 +0530

    Push notifications success

--- server/fcmService.js
+++ server/fcmService.js
@@ -11,6 +11,7 @@ var message = new gcm.Message();
 message.addData('title', 'Hello, World');
 message.addData('body', 'This is a notification that will be displayed ASAP.');
 message.addData('content-available', '1');
+message.addData('force-start', '1');
 
 service.send(message, { registrationTokens: [ deviceID ] }, function (err, response) {
 	if(err) console.error(err);
