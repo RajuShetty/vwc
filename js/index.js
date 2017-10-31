fdd3fdc4b042df90c8074b557431d69faf40f7b9
RajuShetty <rajushetty.b@gmail.com>
2017-10-31 11:49:47 +0530

    onesignal push added

--- js/index.js
+++ js/index.js
@@ -1,121 +1,21 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-		
-		var push = PushNotification.init({
-	android: {
-		 "senderID": "1058053964614"
-		},
-	
-	ios: {
-		alert: "true",
-		badge: "true",
-		sound: "true"
-	},
-	windows: {}
-});
 
-push.on('registration', function(data) {
-	console.log(data.registrationId);
-	var deviceToken = data.registrationId;
-	$.ajax({
-        "url": "http://vineyardworkerschurch.org/push-vwc/",
-        "dataType": "json",
-        "method": "POST",
-        "data": {
-            "device_token" : deviceToken,
-            "device_type" : 'android'
-        },
-        "success": function(response) {
-            console.log("Device ID "+deviceToken+" sent successfuly");
-        }
-    });
-});
+    // Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
 
-push.on('notification', function(data) {
-	data.message,
-	data.title,
-	data.count,
-	data.sound,
-	data.image,
-	data.additionalData
-	console.log(data);
-	navigator.notification.alert(data.additionalData);
-	navigator.notification.confirm( 'Watch live from VWC Church', AlertConfirmed, 'VWC Church', 'Okay!');
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
 
-});
-
-function AlertConfirmed() {
-    window.location = 'liveprayers.html';
-}
-
-push.on('error', function(e) {
-	console.log(e.message);
-});
-		
-
-	
-	push.on('notification', function(data) {
-     console.log('notification event');
-     var cards = document.getElementById("cards");
-     var push = '<div class="swiper-slide slogan bg-slide gradient-container">' +
-       '<div class="fullscreen-title valign-wrapper">' +
-       '  <div class="valign">' +
-       
-       '      <h1 style="color:#ffffff">' + data.title + '</h1>' +
-       '      <p>' + data.message + '</p>' +
-       '      <p>' + data.additionalData.foreground + '</p>' +
-       
-       '  </div>' +
-       ' </div>' +
-       '</div>';
-     cards.innerHTML += push;
-    
-     app.push.finish(function() {
-         console.log('success');
-     }, function() {
-         console.log('error');
-     });
- });	
-		
- 
- 
-        console.log('deviceready event');
-        document.getElementById('regId').innerHTML = 'true';
-		
-    }
-};
-
- app.initialize();
+  window.plugins.OneSignal
+    .startInit("7691ae0f-732d-4a26-86de-b1a327e72ae3")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
